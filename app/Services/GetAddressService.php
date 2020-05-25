c87ad0cf16daaff3da00bbef778087d5047faddd
akandeBolaji <akandbolajidanielofficial@gmail.com>
2020-05-25 19:29:57 +0100

    feature: show backend search result for less than 50km

--- app/Services/GetAddressService.php
+++ app/Services/GetAddressService.php
@@ -3,20 +3,27 @@
 namespace App\Services;
 
 use App\Models\Address;
+use DB;
 
 class GetAddressService
 {
     public function run($request)
     {
-
-        $address = $this->filterAddress(['address'], $request->search);
+        $distance = $this->filterDistance($request->lat, $request->long);
+        $address = $this->filterName($distance, ['address'], $request->search);
 
         return $address;
     }
 
-    protected function filterAddress($field, $parameter)
+    protected function filterName($address, $field, $parameter)
     {
-        return Address::whereLike($field, $parameter)->orderBy('address', 'asc')->get();
+        return $address->whereLike($field, $parameter)->orderBy('address', 'asc')->get();
+    }
+
+    protected function filterDistance($latitude, $longitude)
+    {
+        $address = Address::select(['*', DB::raw('( 0.621371 * 3959 * acos( cos( radians('.$latitude.') ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians('.$longitude.') ) + sin( radians('.$latitude.') ) * sin( radians(latitude) ) ) ) AS distance')])->havingRaw('distance < 500');
+        return $address;
     }
 
 }
--- public/js/app.js
+++ public/js/app.js
@@ -2229,12 +2229,10 @@ __webpack_require__.r(__webpack_exports__);
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _app__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../app */ "./resources/js/app.js");
-/* harmony import */ var _GmapsCubicBezier__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../GmapsCubicBezier */ "./resources/js/GmapsCubicBezier.js");
 //
 //
 //
 //
-
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
@@ -54759,148 +54757,6 @@ module.exports = function(module) {
 	return module;
 };
 
-
-/***/ }),
-
-/***/ "./resources/js/GmapsCubicBezier.js":
-/*!******************************************!*\
-  !*** ./resources/js/GmapsCubicBezier.js ***!
-  \******************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-var map;
-var curvature = 0.5; // how curvy to make the arc
-
-function init(lat1, lng1, lat2, lng2, ref) {
-  var Map = google.maps.Map,
-      LatLng = google.maps.LatLng,
-      LatLngBounds = google.maps.LatLngBounds,
-      Marker = google.maps.Marker,
-      Point = google.maps.Point; // This is the initial location of the points
-  // (you can drag the markers around after the map loads)
-
-  var pos1 = new LatLng(lat1, lng1);
-  var pos2 = new LatLng(lat2, lng2);
-  var bounds = new LatLngBounds();
-  bounds.extend(pos1);
-  bounds.extend(pos2);
-  map = new Map(ref, {
-    center: bounds.getCenter(),
-    zoom: 12
-  });
-  map.fitBounds(bounds);
-  var markerP1 = new Marker({
-    position: pos1,
-    label: "1",
-    draggable: false,
-    map: map
-  });
-  var markerP2 = new Marker({
-    position: pos2,
-    label: "2",
-    draggable: false,
-    map: map
-  });
-  drawCurve(pos2, pos1, map);
-}
-
-function drawCurve(P1, P2, map) {
-  var lineLength = google.maps.geometry.spherical.computeDistanceBetween(P1, P2);
-  var lineHeading = google.maps.geometry.spherical.computeHeading(P1, P2);
-  var lineHeading1, lineHeading2;
-
-  if (lineHeading < 0) {
-    lineHeading1 = lineHeading + 45;
-    lineHeading2 = lineHeading + 135;
-  } else {
-    lineHeading1 = lineHeading + -45;
-    lineHeading2 = lineHeading + -135;
-  }
-
-  var pA = google.maps.geometry.spherical.computeOffset(P1, lineLength / 2.2, lineHeading1);
-  var pB = google.maps.geometry.spherical.computeOffset(P2, lineLength / 2.2, lineHeading2);
-  var curvedLine = new GmapsCubicBezier(P1, pA, pB, P2, 0.01, map);
-} // google.maps.event.addDomListener(window, 'load', initialize);
-// original Belzier Curve code from nicoabie's answer to this question on StackOverflow:
-// http://stackoverflow.com/questions/5347984/letting-users-draw-curved-lines-on-a-google-map
-
-
-var GmapsCubicBezier = function GmapsCubicBezier(latlong1, latlong2, latlong3, latlong4, resolution, map) {
-  var lat1 = latlong1.lat();
-  var long1 = latlong1.lng();
-  var lat2 = latlong2.lat();
-  var long2 = latlong2.lng();
-  var lat3 = latlong3.lat();
-  var long3 = latlong3.lng();
-  var lat4 = latlong4.lat();
-  var long4 = latlong4.lng();
-  var points = [];
-
-  for (it = 0; it <= 1; it += resolution) {
-    points.push(this.getBezier({
-      x: lat1,
-      y: long1
-    }, {
-      x: lat2,
-      y: long2
-    }, {
-      x: lat3,
-      y: long3
-    }, {
-      x: lat4,
-      y: long4
-    }, it));
-  }
-
-  var path = [];
-
-  for (var i = 0; i < points.length - 1; i++) {
-    path.push(new google.maps.LatLng(points[i].x, points[i].y));
-    path.push(new google.maps.LatLng(points[i + 1].x, points[i + 1].y, false));
-  }
-
-  var Line = new google.maps.Polyline({
-    path: path,
-    geodesic: true,
-    strokeColor: "##35495e",
-    strokeOpacity: 0.8,
-    strokeWeight: 3,
-    icons: [{
-      icon: {
-        path: google.maps.SymbolPath.FORWARD_OPEN_ARROW,
-        scale: 5
-      },
-      offset: '100%'
-    }]
-  });
-  Line.setMap(map);
-  return Line;
-};
-
-GmapsCubicBezier.prototype = {
-  B1: function B1(t) {
-    return t * t * t;
-  },
-  B2: function B2(t) {
-    return 3 * t * t * (1 - t);
-  },
-  B3: function B3(t) {
-    return 3 * t * (1 - t) * (1 - t);
-  },
-  B4: function B4(t) {
-    return (1 - t) * (1 - t) * (1 - t);
-  },
-  getBezier: function getBezier(C1, C2, C3, C4, percent) {
-    var pos = {};
-    pos.x = C1.x * this.B1(percent) + C2.x * this.B2(percent) + C3.x * this.B3(percent) + C4.x * this.B4(percent);
-    pos.y = C1.y * this.B1(percent) + C2.y * this.B2(percent) + C3.y * this.B3(percent) + C4.y * this.B4(percent);
-    return pos;
-  }
-};
-/* harmony default export */ __webpack_exports__["default"] = (init);
 
 /***/ }),
 
